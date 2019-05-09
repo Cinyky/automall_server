da952fde63587e0a4fd008c34a79416f2c311be2
Cyy <cyy1079276272@163.com>
2019-05-09 10:43:48 +0800

    init

---
+++ src/main/java/personal/cyy/automall/AutomallApplication.java
@@ -0,0 +1,13 @@
+package personal.cyy.automall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomallApplication.class, args);
+    }
+
+}
